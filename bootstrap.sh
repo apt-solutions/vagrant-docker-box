346fd6a8563da2206241598a5e106224a26a6c54
Denis Huber <huber@apt-solutions.de>
2016-10-24 13:22:48 +0200

    fixed yaml and added virtual folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,24 +6,22 @@ PROJECTFOLDER='docker'
 # create project folder
 sudo mkdir "${PROJECTFOLDER}/wordpress1"
 
-
 # update / upgrade
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-#docker
-echo 1--
+#install docker
 curl -s https://get.docker.com | sudo sh
-echo 2--
 sudo apt-get -y install python-pip
-echo 3--
 sudo pip install docker-compose
 
 #add docker to su group
-sudo groupadd docker
 sudo usermod -aG docker vagrant
 
 #start docker
 service docker start
-docker run hello-world
+
+#start wordpress instance
+cd ${PROJECTFOLDER}/wordpress1
+docker-compose up -d
